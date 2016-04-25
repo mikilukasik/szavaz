171c22b328a5333d70244f4536a2688b4e5fca7d
mikiLukasik <szarhazi@gmail.com>
2016-04-25 06:58:44 +0100

    write some of the canIDo services

--- modules/canIDoServices.js
+++ modules/canIDoServices.js
@@ -1,16 +1,49 @@
 var canIDoServices = function (params) {		//class
 
-	var clientMongoId = params.clientMongoId;
-	var questionId = params.questionId
+	this.clientMongoId = params.clientMongoId;
+	this.questionId = params.questionId
 	
+	this.previousVoteChecked = false;
+	this.previousPromotionChecked = false;
+
+	this.previousVote = '';				//	'yes' or 'no' or ''
+	this.previousPromotion = '';	//	'up' or 'down' or ''
+
+	this.checkPreviousVote = function () {
+		if (previousVoteChecked) return;
+		//TODO: check previous vote with seneca
+
+	};
+	this.checkPreviousPromotion = function () {
+		if (previousPromotionChecked) return;
+		//TODO: check previous promotion with seneca
+
+	};
+
+
   this.alreadyVoted: {
-  	yes: function () {},
-  	no: function () {},
-  },
+  	yes: function () {
+  		this.checkPreviousVote();
+  		return this.previousVote === 'yes';
+  	},
+  	no: function () {
+  		this.checkPreviousVote();
+  		return this.previousVote === 'no';
+
+  	}
+  }
 
   this.alreadyPromoted: {
-  	up: function () {},
-  	down: function () {},
+  	up: function () {
+  		this.checkPreviousPromotion();
+  		return this.previousPromotion === 'up';
+  		
+  	},
+  	down: function () {
+  		this.checkPreviousPromotion();
+  		return this.previousPromotion === 'down';
+  		
+  	}
   },
 
   this.hasEnoughCredit: {
